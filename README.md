1d2f0148357c8488ad7e355195fd8c423aa75af2
Antonin Bas <abas@vmware.com>
2022-09-09 10:12:18 -0700

    Initial code commit
    Signed-off-by: Antonin Bas <abas@vmware.com>

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # migrate-snowflake
-Snowflake database migrations
+
+This is a thin wrapper around
+[golang-migrate](https://github.com/golang-migrate/migrate), which only supports
+Snowflake. This project consumes golang-migrate as a library.
+
+The upstream golang-migrate CLI has the following issues;
+
+ * no support for specifying the Snowflake virtual warehouse to use for queries
+   (this is an issue specific to the provided driver for Snowflake)
+ * credentials must be provided on the command-line as part of the Database URL,
+   with no support for providing them as environment variables
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-logr/logr"
+	"github.com/go-logr/zapr"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/snowflake"
+	sf "github.com/snowflakedb/gosnowflake"
+	"go.uber.org/zap"
+)
+
+var (
+	logger logr.Logger
+
+	source    string
+	database  string
+	schema    string
+	warehouse string
+)
+
+func runMigrations(ctx context.Context, db *sql.DB) error {
+	dbInstance, err := snowflake.WithInstance(db, &snowflake.Config{})
+	if err != nil {
+		return err
+	}
+	m, err := migrate.NewWithDatabaseInstance(source, "snowflake", dbInstance)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			logger.Info("Database is already up-to-date")
+			return nil
+		}
+		return fmt.Errorf("error when running migrations: %w", err)
+	}
+	return nil
+}
+
+func run(ctx context.Context) error {
+	options := make([]func(*sf.Config), 0)
+	if database != "" {
+		options = append(options, SetDatabase(database))
+	}
+	if schema != "" {
+		options = append(options, SetSchema(schema))
+	}
+	if warehouse != "" {
+		options = append(options, SetWarehouse(warehouse))
+	}
+	dsn, _, err := GetDSN(options...)
+	if err != nil {
+		return fmt.Errorf("failed to create DSN: %w", err)
+	}
+
+	db, err := sql.Open("snowflake", dsn)
+	if err != nil {
+		return fmt.Errorf("failed to connect to Snowflake: %w", err)
+	}
+
+	logger.Info("Migrating Snowflake database")
+	if err := runMigrations(ctx, db); err != nil {
+		return err
+	}
+	logger.Info("Migrated Snowflake database")
+
+	return nil
+}
+
+func main() {
+	flag.StringVar(&source, "source", "", "Source for migrations")
+	flag.StringVar(&database, "database", "", "Database to migrate")
+	flag.StringVar(&schema, "schema", "", "Schema to migrate")
+	flag.StringVar(&warehouse, "warehouse", "", "Warehouse to use for queries")
+	flag.Parse()
+
+	if source == "" {
+		log.Fatalf("-source is required")
+	}
+
+	zc := zap.NewProductionConfig()
+	zc.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	zc.DisableStacktrace = true
+	zapLog, err := zc.Build()
+	if err != nil {
+		panic("Cannot initialize Zap logger")
+	}
+	logger = zapr.NewLogger(zapLog)
+	if err := run(context.Background()); err != nil {
+		logger.Error(err, "Failed to migrate database")
+		os.Exit(1)
+	}
+}
